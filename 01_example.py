664d65ef27ace7374f694786fedbd33fda9cafdd
cong235 <nguyenthanhcongsoict@gmail.com>
2019-12-25 17:47:16 +0700

    New Era

--- 01_example.py
+++ 01_example.py
@@ -3,7 +3,7 @@
 # @Project: transform_pytorch_for_tensorflow
 # @Filename: 01_example.py
 # @Last modified by:   cong235
-# @Last modified time: 2019-12-25T17:40:49+07:00
+# @Last modified time: 2019-12-25T17:46:16+07:00
 # @License: MIT
 
 from PIL import Image
@@ -11,7 +11,9 @@ import numpy as np
 import tensorflow as tf
 from transforms_tf import tf_transforms
 
-if tf.version
+#You should activate the eager mode if your TF "more than 0 but less than 2"
+if (tf.__version__.split('.')[0] == 1):
+    tf.enable_eager_execution()
 
 img = Image.open('doge.jpg')
 img_tf = tf.convert_to_tensor(np.transpose(np.asarray(img), (2,0,1)))
